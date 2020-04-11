3f4028c243ca85b3f2ff6fbbc0f10da253c1e680
David Flaig <dflaig@stud.hs-heilbronn.de>
2020-04-11 14:46:36 +0200

    edited the frame

--- TiltingQuest/src/tiltingQuest/frame/Frame.java
+++ TiltingQuest/src/tiltingQuest/frame/Frame.java
@@ -1,13 +1,20 @@
 package tiltingQuest.frame;
 
+import java.awt.Dimension;
+
 import javax.swing.JFrame;
+
+import com.sun.javafx.tk.Toolkit;
 
 public class Frame extends JFrame{
 	
 	public Frame() {
 		
 		setName("Tilting Quest");
-		
+		setSize(800, 700);
+		setLocationRelativeTo(null);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setVisible(true);
 	}
 
 }
